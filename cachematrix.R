ab528fd789447f35ae5fc582d54a4dde2c3bcc85
Chris Winkler <ccwinkler@hotmail.com>
2015-12-26 23:44:24 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+    ## makeCacheMatrix is function which will retrieve data, convert it into a matrix and then calculate the inverse of the matrix 
+## before caching the results(storing in memory)
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  m<-NULL
+  
+  set<-function(y){ ## this function changes the vector stored in the main function
+    x<<-y  ##substitutes the vector x with y 
+    m<<-NULL ##restores the null value to the matrix m; the new matrix needs to be recalculated through the function cacheSolve
+  }
+  get<-function() x  ## gets the matrix
+  setmatrix <-function (matrix) m<<-matrix ## function creates the matrix
+  invertmatrix <-function (solve) x<<-solve ## function inverts the matrix
+  getinvertmatrix <-function() m ## function retrieves the inverted matrix
+  list (get=get, setmatrix=setmatrix, invertmatrix=invertmatrix, getinvertmatrix=getinvertmatrix)
+}  
 
-}
 
-
-## Write a short comment describing this function
+## cacheSolve is a function that will determine if the desired output resides in cached memory prior to running
+## makeCacheMatrix thereby reducing computation time required to unnecessarily run makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <-x$getinvertmatrix()
+  if (!is.null(m)){
+          message("getting cached data") ## this message is displayed if inverted matrix results are already cached
+          return (m) ## returns the output currently in cache
+    }
+    data <- x$get()
+    m <- matrix(data, ...)
+    x$setmatrix(m)
+    m
 }
+            
